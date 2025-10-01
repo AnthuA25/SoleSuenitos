2ea5dc330c593b9c53a1c29d2c5301e8d72355bf
Aylin <aylin_santacruz@hotmail.com>
2025-09-30 22:01:15 -0500

    feat(models):add initial database models

---
+++ back-net/Models/OrdenRollo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace back_net.Models;
+
+public partial class OrdenRollo
+{
+    public int Id { get; set; }
+
+    public int IdOp { get; set; }
+
+    public int IdRollo { get; set; }
+
+    public decimal MetrajeAsignadoM { get; set; }
+
+    public DateTime? FechaAsignacion { get; set; }
+
+    public virtual OrdenProduccion IdOpNavigation { get; set; } = null!;
+
+    public virtual RollosTela IdRolloNavigation { get; set; } = null!;
+}
---
+++ back-net/Models/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace back_net.Models;
+
+public partial class Usuario
+{
+    public int IdUsuario { get; set; }
+
+    public string Correo { get; set; } = null!;
+
+    public string ContrasenaHash { get; set; } = null!;
+
+    public string? NombreCompleto { get; set; }
+
+    public short IdRole { get; set; }
+
+    public DateTime? CreadoEn { get; set; }
+
+    public DateTime? UltimoAcceso { get; set; }
+
+    public bool? Activo { get; set; }
+
+    public virtual ICollection<AnotacionesOpt> AnotacionesOpts { get; set; } = new List<AnotacionesOpt>();
+
+    public virtual ICollection<ComentariosOp> ComentariosOps { get; set; } = new List<ComentariosOp>();
+
+    public virtual Role IdRoleNavigation { get; set; } = null!;
+
+    public virtual ICollection<InspeccionesCalidad> InspeccionesCalidads { get; set; } = new List<InspeccionesCalidad>();
+
+    public virtual ICollection<Molde> Moldes { get; set; } = new List<Molde>();
+
+    public virtual ICollection<Optimizacione> Optimizaciones { get; set; } = new List<Optimizacione>();
+
+    public virtual ICollection<OrdenProduccion> OrdenProduccions { get; set; } = new List<OrdenProduccion>();
+
+    public virtual ICollection<RollosTela> RollosTelas { get; set; } = new List<RollosTela>();
+}
