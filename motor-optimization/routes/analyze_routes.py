7985781657a328d3e6f4e0121f9d3d07d6b61de9
Aylin <aylin_santacruz@hotmail.com>
2025-10-19 21:35:48 -0500

    fix: improve route

--- motor-optimization/routes/analyze_routes.py
+++ motor-optimization/routes/analyze_routes.py
@@ -4,11 +4,12 @@ from core.dxf_reader import leer_dxf
 
 router = APIRouter()
 
-@router.post("/")
+@router.post("/analyze/")
 async def analyze(archivo: UploadFile = File(...)):
-    path = f"output/temp/{archivo.filename}"
-    os.makedirs("output/temp", exist_ok=True)
-    with open(path, "wb") as buffer:
-        buffer.write(await archivo.read())
-    result = leer_dxf(path)
-    return result
+    path = f"temp/{archivo.filename}"
+    os.makedirs("temp", exist_ok=True)
+    with open(path, "wb") as f:
+        f.write(await archivo.read())
+
+    data = leer_dxf(path)
+    return {"resultado": data}
