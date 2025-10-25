bc6df6a87fa344a796fb83ef7221e533b935beff
Aylin <aylin_santacruz@hotmail.com>
2025-10-24 19:52:43 -0500

    configuration

--- back-net/Models/AppDbContext.cs
+++ back-net/Models/AppDbContext.cs
@@ -36,7 +36,7 @@ public partial class AppDbContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseNpgsql("Host=dpg-d3e4sb0gjchc738bhepg-a.oregon-postgres.render.com;Port=5432;Database=syscorte;Username=admin;Password=MQEpjFPpqZHSjgLwOUqnZjQAxY0bWD1q;Ssl Mode=Require;Trust Server Certificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
